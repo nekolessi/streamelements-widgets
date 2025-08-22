62ed889ed9126a8a419b8bb1300f69e624885815
nekolessi <nekolessi8@gmail.com>
2025-08-21 23:29:07 -0500

    Fixing all the stuffs...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 import js from '@eslint/js';
 import pluginImport from 'eslint-plugin-import';
 import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   js.configs.recommended,
   {
     plugins: { import: pluginImport },
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node,
         ...globals.browser,
@@ -15,13 +19,28 @@ export default [
     rules: {
       'import/order': ['warn', { 'newlines-between': 'always' }],
       'curly': ['error', 'all'],
-      'no-console': ['warn', { allow: ['warn', 'error'] }]
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: false,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
     },
   },
   {
     files: ['vitest.config.mjs'],
     rules: {
-      'import/no-unresolved': 'off'
-    }
-  }
+      'import/no-unresolved': 'off',
+    },
+  },
 ];
